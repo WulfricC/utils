6e5b74807f340124fa08852613b3c765526cb493
Wulfric K R Clackson <wulfric.clackson@gmail.com>
2022-07-02 23:49:36 +0100

    removed unnessecary comments

--- buffer-utils.js
+++ buffer-utils.js
@@ -1,3 +1,3 @@
 export function bufferString(arrayBuffer) {
-    return [...new Uint8Array(arrayBuffer)].map(v => /*v.toString().padStart(3,'0') + */ v > 32 && v < 127 ? v >= 127 ? '-' : String.fromCharCode(v) : '_').join('');
+    return [...new Uint8Array(arrayBuffer)].map(v => v > 32 && v < 127 ? String.fromCharCode(v) : '_').join('');
 }
